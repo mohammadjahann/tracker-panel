682b4f1ed1a9db51e08ece38ec0ac329cf18b51d
Jahan <you@example.com>
2025-12-28 22:42:46 +0330

    Add animation to Login Page Modals

--- src/Components/FailedLogin.jsx
+++ src/Components/FailedLogin.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { MdClose } from "react-icons/md";
 
-const FailedLogin = ({closeModal}) => {
+
+const FailedLogin = ({ closeModal }) => {
+
+  const [animate, setAnimate] = useState(false)
+
+  useEffect(() => {
+
+    setAnimate(true)
+
+    return () => {
+      setAnimate(false)
+    }
+  }, [])
   return (
-    <div className=' w-[400px] h-20 p-2 bg-white/30 absolute right-2 top-3 flex flex-col justify-center items-center rounded-md text-white '>
-        <h4>متاسفانه موفق به ورود نشدید</h4>
-        <p>مجدد تلاش کنید</p>
-        
-        <MdClose fontSize={'20px'} fontWeight={'6'} onClick={closeModal} className=' cursor-pointer absolute right-2 top-2' />
+    <div className={`w-[400px] h-20 p-2 bg-white/30 absolute right-2 top-3 flex flex-col justify-center items-center rounded-md text-white ${animate ? 'translate-x-0' : 'translate-x-full'} transition-transform duration-200`}>
+      <h4>متاسفانه موفق به ورود نشدید</h4>
+      <p>مجدد تلاش کنید</p>
+
+      <MdClose fontSize={'20px'} fontWeight={'6'} onClick={closeModal} className=' cursor-pointer absolute right-2 top-2' />
     </div>
   )
 }
--- src/Components/SuccssesLogIn.jsx
+++ src/Components/SuccssesLogIn.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const SuccssesLogIn = () => {
-    return (
-        <div className=' w-[400px] h-20 p-2 bg-white/30 absolute right-2 top-3 flex flex-col justify-center items-center rounded-md text-white '>
-            <h4>ورود به پنل با موفقیت انجام شد</h4>
-         
-        </div>
-    )
+  const [animate, setAnimate] = useState(false)
+
+  useEffect(() => {
+    
+    setAnimate(true)
+  }, [])
+
+  return (
+    <div
+      className={`w-[400px] h-20 p-2 bg-white/30 absolute right-2 top-3 flex flex-col justify-center items-center rounded-md text-white transition-transform duration-300 ease-out ${animate ? 'translate-x-0' : 'translate-x-full'}`}
+    >
+      <h4>ورود به پنل با موفقیت انجام شد</h4>
+    </div>
+  )
 }
 
 export default SuccssesLogIn
